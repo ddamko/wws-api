b1b8b6151b07aa05ccef24a369643b53654e167a
Derek Damko <ddamko@wwsires.com>
2023-08-30 16:28:46 -0700

    Removed old files

--- src/controllers/animal.ts
+++
@@ -1,6 +0,0 @@
-import AnimalService from "@services/animal"
-
-export const GetAnimalById = async (ctx: { params: { id: string } }) => {
-    const animalService = new AnimalService();
-    return await animalService.GetAnimalById(ctx.params.id);
-}
