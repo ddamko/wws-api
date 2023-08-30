f87bcf616ed2745d62f2c2c32a89b5881eeb5646
Derek Damko <ddamko@wwsires.com>
2023-08-30 16:28:32 -0700

    Added new routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Elysia, t } from "elysia";
-import Datebase from "@database/database";
-import { GetAnimalById } from "@controllers/animal";
+import { GetBull, GetBullHealth, GetBullLineage } from "@controllers/bull-controller";
 
 const app = new Elysia().listen(3000);
 
-app.get("/animal/:id", GetAnimalById, { params: t.Object({ id: t.String() }) })
-
+app.get("/bull/:id", GetBull, { params: t.Object({ id: t.String() }) });
+app.get("/bull/health/:id", GetBullHealth, { params: t.Object({ id: t.String() }) });
+app.get("/bull/lineage/:id", GetBullLineage, { params: t.Object({ id: t.String() }) });
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
