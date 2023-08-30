b91839155de2b291c3e33c16cf2667851a0ad41a
Derek Damko <derek@damkostudios.com>
2023-08-30 06:49:30 -0700

    Adding methods to the new Bull Service

---
+++ src/services/bull-service.ts
@@ -0,0 +1,24 @@
+import Database from "@database/database";
+
+class BullService {
+
+    db: Database;
+
+    constructor() {
+        this.db = new Database();
+    }
+
+    async GetBullById(id: string) {
+        
+    }
+
+    async GetBullLineage(id: string) {
+
+    }
+
+    async GetBullMarketingRights(id: string) {
+        
+    }
+
+
+}
