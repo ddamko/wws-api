861663616b054062f3154cf0e439df6f3f9f7403
Derek Damko <ddamko@wwsires.com>
2023-08-31 16:32:11 -0700

    Testing resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import { Elysia, t } from "elysia";
 import { GetBull, GetBullHealthTests, GetBullLineage } from "@controllers/bull-controller";
+import BullService from "@services/bull-service";
 import { apollo, gql } from '@elysiajs/apollo'
 import { readFileSync } from 'node:fs'
-import { Resolvers } from "resolvers/resolvers-types";
+import { Bull, Resolvers } from "resolvers/resolvers-types";
 
 // Define the app an listen on port 3000.
 const app = new Elysia().listen(3000);
 // Import GraphQL schema.
-const typeDefs = readFileSync('./schema/schema.graphql', 'utf8');
-
+const typeDefs = readFileSync('./src/schema/schema.graphql', 'utf8');
+// Define resolvers.
 const resolvers: Resolvers = {
     Query: {
-        GetBull: async (_: any, { id }: { id: any }) => {
-            return { GlobalId: "", RegId: "", Gender: "" }
+        GetBull: async (_, { id }: { id: string }) => {
+            const bullService = new BullService();
+            const result = await bullService.GetBullDataByCategory<Bull>(id, "base");
+            return result[0];
         }
     }
 }
