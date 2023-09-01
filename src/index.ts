04d7da3af0d2bcf8049db12165c3e6f5ba86e98b
Derek Damko <ddamko@wwsires.com>
2023-09-01 16:14:10 -0700

    Updated with new resolvers.

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,60 @@
 import { Elysia, t } from "elysia";
-import { GetBull, GetBullHealthTests, GetBullLineage } from "@controllers/bull-controller";
-import BullService from "@services/bull-service";
+// import { GetBull, GetBullHealthTests, GetBullLineage } from "@controllers/bull-controller";
 import { apollo } from '@elysiajs/apollo'
 import { readFileSync } from 'node:fs'
-import { Bull, Resolvers, Sons } from "resolvers/resolvers-types";
+import { Resolvers } from "@app-types/gen-types";
+import {
+    BullDamProductionResolver,
+    BullDesignationsResolver,
+    BullGenotypesResolver,
+    BullHaplotypesResolver,
+    BullHeathTestsResolver,
+    BullIdentityResolver,
+    BullLineageResolver,
+    BullMarketingGroupsResolver,
+    BullMetaResolver,
+    BullNaabCodesResolver,
+    BullProdStateResolver,
+    BullProofHistoryResolver,
+    BullProofResolver,
+    BullProofSourceResolver,
+    BullResolver,
+    BullSonsResolver
+} from "@resolvers/bull-resolver";
 
 // Define the app an listen on port 3000.
 const app = new Elysia().listen(3000);
 // Import GraphQL schema.
 const typeDefs = readFileSync('./src/schema/schema.graphql', 'utf8');
-// Instantiate BullService.
-const bullService = new BullService();
 // Define resolvers.
 const resolvers: Resolvers = {
     Query: {
-        Bull: async (_, { id }: { id: string }) => {
-            const result = await bullService.GetBullDataByCategory<Bull>(id, "base");
-            return result[0];
-        },
-        Sons: async (_, { id }: { id: string }) => {
-            const result = await bullService.GetBullDataByCategory<Sons>(id, "sons");
-            return result as Sons[];
-        }
-        
+        Bull: BullResolver
+    },
+    Bull: {
+        Sons: BullSonsResolver,
+        Lineage: BullLineageResolver,
+        HealthTests: BullHeathTestsResolver,
+        NaabCodes: BullNaabCodesResolver,
+        Haplotypes: BullHaplotypesResolver,
+        Designations: BullDesignationsResolver,
+        MarketingGroups: BullMarketingGroupsResolver,
+        DamProduction: BullDamProductionResolver,
+        Meta: BullMetaResolver,
+        Genotypes: BullGenotypesResolver,
+        ProdState: BullProdStateResolver,
+        Identities: BullIdentityResolver,
+        ProofHistory: BullProofHistoryResolver,
+        ProofSource: BullProofSourceResolver,
+        Proof: BullProofResolver
     }
 }
 
 app.use(apollo({ typeDefs: typeDefs, resolvers: resolvers }));
 
-app.get("/bull/:id", GetBull, { params: t.Object({ id: t.String() }) });
-app.get("/bull/health/:id", GetBullHealthTests, { params: t.Object({ id: t.String() }) });
-app.get("/bull/lineage/:id", GetBullLineage, { params: t.Object({ id: t.String() }) });
+// app.get("/bull/:id", GetBull, { params: t.Object({ id: t.String() }) });
+// app.get("/bull/health/:id", GetBullHealthTests, { params: t.Object({ id: t.String() }) });
+// app.get("/bull/lineage/:id", GetBullLineage, { params: t.Object({ id: t.String() }) });
 
 console.log(
     `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
